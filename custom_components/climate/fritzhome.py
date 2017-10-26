6bfe807344794c6b09f5a165e4a131eb3179ba3c
Heiko Thiery <heiko.thiery@gmail.com>
2017-10-26 09:01:56 +0200

    fritzhome/climate: add set temp and some attributes
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

--- custom_components/climate/fritzhome.py
+++ custom_components/climate/fritzhome.py
@@ -2,9 +2,10 @@ import logging
 
 from custom_components.fritzhome import DATA_FRITZHOME
 from homeassistant.components.climate import (
-    ClimateDevice, PRECISION_HALVES, STATE_ECO,
+    ClimateDevice, ENTITY_ID_FORMAT, PRECISION_HALVES, STATE_ECO,
 )
-from homeassistant.const import (TEMP_CELSIUS,)
+from homeassistant.const import (TEMP_CELSIUS, ATTR_TEMPERATURE)
+from homeassistant.helpers.entity import Entity, async_generate_entity_id
 
 DEPENDENCIES = ['fritzhome']
 
@@ -14,6 +15,10 @@ STATE_COMFORT = 'comfort'
 
 OPERATION_LIST = [STATE_ECO, STATE_COMFORT]
 
+ATTR_FW_VERSION = 'firmware_verson'
+ATTR_MANUFACTURER = 'manufacturer'
+ATTR_PRODUCTNAME = 'product_name'
+
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set the Fritzhome thermostat platform."""
@@ -21,16 +26,13 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     if DATA_FRITZHOME not in hass.data:
         return False
 
-    _LOGGER.warning("setup_platform climate")
-
     fritz = hass.data[DATA_FRITZHOME]
     device_list = fritz.get_devices()
 
     devices = []
     for device in device_list:
-        _LOGGER.warning(device.name)
         if device.has_thermostat:
-            devices.append(FritzhomeThermostat(device))
+            devices.append(FritzhomeThermostat(hass, device))
 
     add_devices(devices)
 
@@ -38,21 +40,24 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class FritzhomeThermostat(ClimateDevice):
     """The thermostat class for Fritzhome."""
 
-    def __init__(self, device):
+    def __init__(self, hass, device):
         self._device = device
         self._temperature = None
         self._target_temperature = None
         self._eco_temperature = None
         self._comfort_temperature = None
 
+        self.entity_id = async_generate_entity_id(ENTITY_ID_FORMAT, device.name,
+                                                  hass=hass)
+
     @property
     def available(self):
         """Return if thermostat is available."""
         return self._device.get_present()
 
-#    @property
-#    def should_poll(self):
-#        return True
+    @property
+    def should_poll(self):
+        return True
 
     @property
     def name(self):
@@ -79,12 +84,12 @@ class FritzhomeThermostat(ClimateDevice):
         """Return the temperature we try to reach."""
         return self._target_temperature
 
-#    def set_temperature(self, **kwargs):
-#        """Set new target temperature."""
-#        temperature = kwargs.get(ATTR_TEMPERATURE)
-#        if temperature is None:
-#            return
-#        self._thermostat.target_temperature = temperature
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        if temperature is None:
+            return
+        self._device.set_temperature(temperature)
 
     @property
     def current_operation(self):
@@ -129,18 +134,17 @@ class FritzhomeThermostat(ClimateDevice):
         """Return the maximum temperature."""
         return self._comfort_temperature
 
-#    @property
-#    def device_state_attributes(self):
-#        """Return the device specific state attributes."""
-#        dev_specific = {
-#            ATTR_STATE_LOCKED: self._thermostat.locked,
-#            ATTR_STATE_LOW_BAT: self._thermostat.low_battery,
-#            ATTR_STATE_VALVE: self._thermostat.valve_state,
-#            ATTR_STATE_WINDOW_OPEN: self._thermostat.window_open,
-#            ATTR_STATE_AWAY_END: self._thermostat.away_end,
-#        }
-#
-#        return dev_specific
+    @property
+    def device_state_attributes(self):
+        """Return the device specific state attributes."""
+        dev_specific = {
+            ATTR_FW_VERSION : self._device.fw_version,
+            ATTR_MANUFACTURER : self._device.manufacturer,
+            ATTR_PRODUCTNAME: self._device.productname,
+            ATTR_PRODUCTNAME: self._device.productname,
+        }
+
+        return dev_specific
 
     def update(self):
         """Update the data from the thermostat."""
